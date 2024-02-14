2cd6c5c1d6844c1b796ccc94c336632b05fdcd66
satyarthsrivatava <143537599+satyarthsrivatava@users.noreply.github.com>
2024-02-14 12:07:41 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//document.getElementById("count-el").innerText=5;
+let c = 0;
+function inc(){
+    c++;
+    console.log(c);
+    //$("#count-el").hide();
+    document.getElementById("count-el").innerText=c;
+}
